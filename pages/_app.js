a7b4a49b4e586b98f3e3ee29e4128f08dae36163
MadiMagpie <mcafikirwin@gmail.com>
2023-02-20 23:45:27 -0800

    font fix

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,9 @@
 import NavBar from '../components/NavBar'
 import '../styles/globals.css'
-import localFont from '@next/font/local'
-const anonymousPro = localFont({src: '../fonts/AnonymousPro-Regular.ttf'})
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-    <style jsx global>{
-      `html{
-        font-family: ${anonymousPro};
-      }`
-    }</style>
     <Component {...pageProps} />
     <NavBar/>
   </>
